6872eb8941c742a11a333c87b47672270d30e90d
Mike King <zealinux@gmail.com>
2018-08-13 15:16:53 +0800

    refact: streamline MocMvc test

--- src/test/java/com/example/demo/HelloControllerTest.java
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -4,31 +4,25 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = DemoApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
+@WebMvcTest(HelloController.class)
 public class HelloControllerTest {
 
   /**
    * Test with MocMvc
    */
 
+  @Autowired
   private MockMvc mvc;
-
-  @Before
-  public void setUp() {
-    mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
-  }
 
   @Test
   public void testHelloWithMoc() {
